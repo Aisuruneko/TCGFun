6df78d4fee4c0350d620099c06ff1f2c0d6c65f1
TheCodingGuy <matthewmartin989@gmail.com>
2020-05-29 23:37:24 -0400

    First commit! We create the plugin! (What else would this be?)

---
+++ src/main/java/ml/officialtcgmatt/tcgfun/BeaconStuff.java
@@ -0,0 +1,20 @@
+package ml.officialtcgmatt.tcgfun;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BeaconStuff extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO:
+        // Enable stuff to console
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Command stuff :thonk:
+        return true;
+    }
+}
